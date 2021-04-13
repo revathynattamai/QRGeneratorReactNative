da22c0626b3d0754ae099f30b66aa236a8a71e27
Nattamai Gopinath <r.nattamai.gopinath@accenture.com>
2021-04-13 13:12:43 +0530

    Functionality completed

--- src/screens/HomeScreen.js
+++ src/screens/HomeScreen.js
@@ -1,13 +1,75 @@
-import React from "react";
-import { Text, StyleSheet } from "react-native";
+import React, { useState } from "react";
+import { Alert, Modal, StyleSheet, Text, TouchableOpacity, View } from "react-native";
+import QRCode from 'react-native-qrcode-svg';
 
 const HomeScreen = () => {
-  return <Text style={styles.text}>HomeScreen</Text>;
+
+  const [modalVisible, setModalVisible] = useState(false);
+  return (
+    <View>
+      <Modal
+        animationType="slide"
+        transparent={true}
+        visible={modalVisible}
+        onRequestClose={() => {
+          setModalVisible(!modalVisible);
+        }}
+      >
+        <View style={styles.centeredView}>
+          <View style={styles.modalView}>
+            <View style={{ flexDirection: 'row', justifyContent: "space-between", marginBottom: 28 }}>
+              <Text>QR Code</Text>
+              <TouchableOpacity
+                onPress={() => setModalVisible(!modalVisible)}
+              >
+                <Text style={styles.textStyle}>X</Text>
+              </TouchableOpacity>
+            </View>
+            <QRCode
+              value={JSON.stringify({ "a": "a", "b": "b", "date": new Date() })}
+              color={"black"}
+              size={200}
+            />
+          </View>
+        </View>
+      </Modal>
+      <TouchableOpacity
+        onPress={() => setModalVisible(true)}
+      >
+        <Text>Generate a QR code</Text>
+      </TouchableOpacity>
+    </View>
+  );
 };
 
+
 const styles = StyleSheet.create({
-  text: {
-    fontSize: 30
+  centeredView: {
+    flex: 1,
+    justifyContent: "center",
+    alignItems: "center",
+    // marginTop: 22
+    padding: 10,
+    // fontSize: 16
+  },
+  modalView: {
+    margin: 20,
+    backgroundColor: "white",
+    borderRadius: 20,
+    padding: 35,
+    shadowColor: "#000",
+    shadowOffset: {
+      width: 0,
+      height: 2
+    },
+    shadowOpacity: 0.25,
+    shadowRadius: 4,
+    elevation: 5
+  },
+  textStyle: {
+    color: "black",
+    fontWeight: "bold",
+    textAlign: "right",
   }
 });
 
